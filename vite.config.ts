f1f18f3b82fd926d9d03522092134823354cfeb3
Dawid Skinder <kontakt@dawidskinder.pl>
2026-02-25 13:59:51 +0000

    Blank page fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/dawidskinder-pl-2026/',
+  base: '/dawidskinder.pl-2026/',
   plugins: [react()],
   server: {
     host: true,
